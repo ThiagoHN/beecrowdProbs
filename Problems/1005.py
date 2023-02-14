2abfe70063372bf0c343b2a75dea38d31aec12d4
ThiagoHN <thiagohnogueira01@gmail.com>
2023-02-13 23:54:36 -0300

    [feat][python] Problems 1005 and 1006 done

---
+++ Problems/1005.py
@@ -0,0 +1,7 @@
+def mean(A,B):
+    return (A+B)/11
+
+A = float(input()) * 3.5 
+B = float(input()) * 7.5
+
+print("MEDIA = ", format(mean(A,B), '.5f'), sep='')
---
+++ Problems/1006.py
@@ -0,0 +1,8 @@
+def mean(A,B,C):
+    return (A+B+C)/10
+
+A = float(input()) * 2 
+B = float(input()) * 3
+C = float(input()) * 5
+
+print("MEDIA = ", format(mean(A,B,C), '.1f'), sep='')
